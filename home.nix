61a0ca39801902d1f52a3dc89593d22317dc5440
Daniel Fabian <daniel.fabian@integral-it.ch>
2019-03-24 15:29:38 +0000

    Comments and xmonad config.

--- home.nix
+++ home.nix
@@ -3,14 +3,18 @@
 {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  
+  # terminal emulator: rxvt-unicode
   programs.urxvt = {
     enable = true;
     fonts = [ "xft:Terminus:pixelsize=16" ];
     keybindings = {
+      # allow some copy/paste
       "Shift-Control-C" = "eval:selection_to_clipboard";
       "Shift-Control-V" = "eval:paste_clipboard";
     };
     extraConfig = {
+      # dark (transparent) background without eye-fucking blues.
       background = "[35]#000000";
       foreground = "White";
       color4 = "RoyalBlue";
@@ -18,19 +22,36 @@
     };
   };
 
-  home.packages = [
-    pkgs.terminus_font
-    pkgs.wget
-    pkgs.pastebinit
+  home.packages = with pkgs; [
+    # fonts for terminal, etc.
+    terminus_font
+
+    # downloading from the net
+    wget
+
+    # uploading console output to pastebin
+    pastebinit
   ];
 
+  # git config
   programs.git = {
     enable = true;
     userName = "Daniel Fabian";
     userEmail = "daniel.fabian@integral-it.ch";
   };
 
+  # process viewer: htop
   programs.htop.enable = true;
+
+  # ide: VS code
   programs.vscode.enable = true;
 
+  # control XSession from within home-manager:
+  xsession = {
+    enable = true;
+    windowManager.xmonad = {
+      enable = true;
+      config = ./xmonad/xmonad.hs;
+    };
+  };
 }
