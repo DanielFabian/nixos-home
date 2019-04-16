daceed927979169938488c962ccf211124d0ee11
Daniel Fabian <daniel.fabian@integral-it.ch>
2019-04-16 09:16:26 +0100

    Add keybindings for xmonad.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -11,13 +11,15 @@ import XMonad.Layout.MultiColumns
 modKey = mod4Mask
 
 myKeys =
+    ("M-p", spawn "rofi -show run"):
+    ("M-e", spawn "brave"):
+    ("M-c", spawn "code"):
     -- moving floating window with key
     [(c ++ m ++ k, withFocused $ f (d x))
          | (d, k) <- zip [\a->(a, 0), \a->(0, a), \a->(0-a, 0), \a->(0, 0-a)] ["<Right>", "<Down>", "<Left>", "<Up>"]
          , (f, m) <- zip [keysMoveWindow, \d -> keysResizeWindow d (0, 0)] ["M-", "M-S-"]
          , (c, x) <- zip ["", "C-"] [20, 2]         
-    ] ++
-    [("M-p", spawn "rofi -show run")]
+    ]
 
 myLayouts = multiCol [2] 3 0.01 (-0.5) ||| layoutHook def
 
