a625b14402de7895210e937ecc4b9a90a5ae2702
Daniel Fabian <daniel.fabian@integral-it.ch>
2019-05-08 09:33:33 +0100

    Change the way how we swap caps lock and escape. Use Driver modification instead of using an X.org configuration. This fixes RDP connections!

--- system-modules/internationalization.nix
+++ system-modules/internationalization.nix
@@ -9,8 +9,18 @@ in
   };
 
   # make xkbConfig happy too
-  services.xserver = {
-    layout = hmConfig.home.keyboard.layout;
-    xkbOptions = builtins.concatStringsSep ", " hmConfig.home.keyboard.options;
-  };
+  services = {
+    xserver = {
+      layout = hmConfig.home.keyboard.layout;
+      xkbOptions = builtins.concatStringsSep ", " hmConfig.home.keyboard.options;
+    };
+
+    # remap caps lock and escape so that we can use vim key bindings globally.
+    udev.extraHwdb = ''
+      # Razer BlackWidow Chroma
+      evdev:input:b0003v1532p0203*
+       KEYBOARD_KEY_70029=capslock
+       KEYBOARD_KEY_70039=esc
+      '';
+    };
 }
