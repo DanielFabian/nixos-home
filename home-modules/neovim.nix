3f54f5bbb32562ed90d07c55f56d8e4ac9ef2271
Daniel Fabian <daniel.fabian@integral-it.ch>
2020-01-27 17:41:37 +0000

    Change indentation settings in vimrc.

--- home-modules/neovim.nix
+++ home-modules/neovim.nix
@@ -10,6 +10,10 @@
         if $TERM !~ 'linux'
           let g:airline_powerline_fonts = 1
         endif
+        set autoindent
+        set shiftwidth=2
+        set softtabstop=2
+        set expandtab
         '';
       packages.myVimPackages = with pkgs.vimPlugins; {
         start = [ vim-nix vim-airline vim-airline-themes ];
