6bd7b1ab5f919c332f6a67e7f048a3ab33131d5a
Daniel Fabian <daniel.fabian@integral-it.ch>
2020-01-27 22:53:07 +0000

    Add support for fish-completions for azure.

--- home-modules/fish.nix
+++ home-modules/fish.nix
@@ -8,4 +8,7 @@
       fish_vi_cursor
       '';
   };
+
+  xdg.dataFile."fish/generated_completions/az.fish".source = 
+      pkgs.runCommand "az.fish" {} "${pkgs.python38Packages.argcomplete}/bin/register-python-argcomplete --shell fish az > $out";
 }
