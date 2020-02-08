0fbe8eaae2656e8c903fa9bee09471deb986ecd8
Daniel Fabian <daniel.fabian@integral-it.ch>
2020-02-08 09:46:09 +0000

    Move stateVersion out of the global shared config into a new one, because it should never change. If this variable is set when a Nixos is initially installed and remains fixed afterwards.

--- system-modules/shared.nix
+++ system-modules/shared.nix
@@ -22,12 +22,6 @@
     sound.enable = true;
     hardware.pulseaudio.enable = true;
 
-    # This value determines the NixOS release with which your system is to be
-    # compatible, in order to avoid breaking some software such as database
-    # servers. You should change this only after NixOS release notes say you
-    # should.
-    system.stateVersion = "19.09"; # Did you read the comment?
-
     # globally allow unfree software, needed for drivers, etc.
     nixpkgs.config.allowUnfree = true;
 
--- systems/dany-pc/configuration.nix
+++ systems/dany-pc/configuration.nix
@@ -39,4 +39,11 @@ with lib;
   networking.hostName = "dany-pc"; # Define your hostname.
 
   services.xserver.videoDrivers = [ "nvidia" ];
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09"; # Did you read the comment?
+
 }
