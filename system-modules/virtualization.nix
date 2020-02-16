112200c1c931d207dcb598486885f145961f1194
Daniel Fabian <daniel.fabian@integral-it.ch>
2020-02-16 14:42:06 +0000

    Add VirtualBox.

--- system-modules/virtualization.nix
+++ system-modules/virtualization.nix
@@ -5,7 +5,9 @@
     ];
 
     virtualisation.libvirtd.enable = true;
+    virtualisation.virtualbox.host.enable = true;
+    virtualisation.virtualbox.host.enableExtensionPack = true;
     boot.kernel.sysctl = { "net.ipv4.ip_forward" = 1; };
 
-    users.users.dany.extraGroups = [ "libvirtd" ];
+    users.users.dany.extraGroups = [ "libvirtd" "vboxusers" ];
 }
