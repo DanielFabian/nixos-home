78b0e769e98e011ffcec26e572211af9355e9f0e
Daniel Fabian <daniel.fabian@integral-it.ch>
2020-03-22 11:13:19 +0000

    Add icons for lutris.

--- system-modules/x11.nix
+++ system-modules/x11.nix
@@ -1,5 +1,15 @@
-{...}:
+{pkgs, ...}:
 {
+  xdg.icons.enable = true;
+  environment.systemPackages = [ pkgs.gnome3.adwaita-icon-theme pkgs.gnome3.gnome-themes-extra ];
+  qt5 = { 
+    enable = true;
+    platformTheme = "gnome";
+    style = "adwaita";
+  };
+
+  gtk.iconCache.enable = true;
+
   # Enable the X server.
   services.xserver = {
       enable = true;
