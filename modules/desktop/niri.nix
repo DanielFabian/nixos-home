f23be3abb4d9a64a5f698d02aa1f0141dfce1275
Dany Fabian <danfab@microsoft.com>
2026-02-04 21:34:03 +0000

    xdg-desktop-portal-kde

--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -3,10 +3,26 @@
 { config, pkgs, ... }:
 
 {
-  # Enable Niri compositor (nixpkgs module handles portals)
+  # Enable Niri compositor (nixpkgs module handles base portals)
   programs.niri = {
     enable = true;
     useNautilus = false; # Use GTK portal for file dialogs
+  };
+
+  # FIX: portal-gnome doesn't implement OpenURI, only portal-kde does
+  xdg.portal = {
+    extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
+    config.niri = {
+      # KDE portal for OpenURI (the only one that implements it!)
+      "org.freedesktop.impl.portal.OpenURI" = "kde";
+      # GTK for file dialogs (already set by programs.niri, but be explicit)
+      "org.freedesktop.impl.portal.FileChooser" = "gtk";
+      # Fallback
+      default = [
+        "kde"
+        "gtk"
+      ];
+    };
   };
 
   # Minimal login manager
